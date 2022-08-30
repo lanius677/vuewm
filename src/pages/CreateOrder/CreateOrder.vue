09e7ff490da3d7fc4066b99492e9c4ce615c7c84
lanius <lanius@foxmail.com>
2022-08-30 23:30:14 +0800

    完成生成订单页面

--- src/pages/CreateOrder/CreateOrder.vue
+++ src/pages/CreateOrder/CreateOrder.vue
@@ -1,24 +1,94 @@
 <template>
-  <div class="orderContainer">
+  <div class="createOrderContainer">
     <Header title="生成订单"></Header>
-    <van-contact-card type="edit" :tel="data.tel" :name="data.name" @click="onEdit" />
+    <van-contact-card type="edit" :tel="data.currentContact.tel" :name="data.currentContact.name" @click="onEdit" />
+
+    <div class="content">
+      <div class="" v-for="(i, index) in store.state.orderList" :key="index">
+        <van-card :num="i.num" :price="i.price" :title="i.title" :thumb="i.pic" />
+      </div>
+    </div>
+
+    <div class="payWrap">
+      <div class="price">
+        <span>商品金额</span>
+        <span>￥{{initPrice}}</span>
+      </div>
+      <van-button class="payButton" type="primary" @click="handleCreateOrder" block>生成订单</van-button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import Header from '@/components/Header.vue';
-import { reactive } from 'vue';
-
-
+import { computed, onMounted, reactive } from 'vue';
+import { useStore } from 'vuex';
+const store = useStore()
 const data = reactive({
-  name: '677',
-  tel: '123123123',
+  currentContact: {
+    name: '677',
+    tel: '123123123',
+  },
+
+  allPrice:0
 })
 
+interface List{
+  price:number,
+  num:number
+}
+
+//地址编辑按钮
 const onEdit = () => {
 
 }
+
+//生成订单按钮
+const handleCreateOrder=()=>{
+
+}
+
+const initPrice=computed(()=>{
+  let price:number=0
+  if(store.state.orderList.length){
+    store.state.orderList.map((item:List)=>{
+      price+=item.num*item.price
+    })
+  }
+ return data.allPrice=price
+})
+
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.createOrderContainer{
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+  .content{
+    flex: 1;
+    overflow: auto;
+  }
+  .payWrap{
+    padding: 10px 0;
+    widows: 100%;
+    background: #fff;
+    border-top: 1px solid #e9e9e9;
+    > div{
+      display: flex;
+      justify-content: space-between;
+      padding: 0 5%;
+      margin: 10px 0;
+      font-size: 14px;
+      span:nth-child(2){
+        color: red;
+        font-size: 18px;
+      }
+    }
+    .payButton{
+      width:90%;
+      margin: 0 auto;
+    }
+  }
+}
 </style>
